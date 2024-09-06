df30265bb9c7a19b7d697c467af59042510e3f9f
ROCKY <riazulrocky@gmail.com>
2024-09-06 18:35:48 +0600

    first commit

--- lib/login.dart
+++ lib/login.dart
@@ -10,6 +10,14 @@ class login extends StatefulWidget {
 class _loginState extends State<login> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+            image: DecorationImage(
+              fit: BoxFit.cover,
+                image: NetworkImage(
+                    "https://img.freepik.com/premium-photo/colorful-image-blue-pink-wave-that-says-blue_1034303-453873.jpg"))),
+      ),
+    );
   }
 }
