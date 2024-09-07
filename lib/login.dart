70e3d23b40c42ef6f60d7705187c36d5a87dd14f
ROCKY <riazulrocky@gmail.com>
2024-09-07 13:21:42 +0600

    first commit

--- lib/login.dart
+++ lib/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:login_ui_with_validation/home.dart';
 
 class login extends StatefulWidget {
   const login({super.key});
@@ -8,6 +9,13 @@ class login extends StatefulWidget {
 }
 
 class _loginState extends State<login> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  bool isObscure = true;
+
+  GlobalKey<FormState> formkey = GlobalKey();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,59 +50,81 @@ class _loginState extends State<login> {
               ),
               child: Padding(
                 padding: const EdgeInsets.only(top: 40),
-                child: Column(
-                  children: [
-                    TextFormField(
-                      decoration: InputDecoration(
-                          prefix: Icon(Icons.email),
-                          hintText: "Enter your email",
+                child: Form(
+                  key: formkey,
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return "Email can not be empty!";
+                          }
+                        },
+                        controller: emailController,
+                        decoration: InputDecoration(
+                            prefix: Icon(Icons.email),
+                            hintText: "Enter your email",
+                            border: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.blue),
+                                borderRadius: BorderRadius.circular(60))),
+                      ),
+                      SizedBox(height: 20),
+                      TextFormField(
+                        controller: passwordController,
+                        obscureText: isObscure,
+                        decoration: InputDecoration(
+                          prefix: Icon(Icons.lock),
+                          suffix: IconButton(
+                              onPressed: () {
+                                setState(() {
+                                  isObscure = !isObscure;
+                                });
+                              },
+                              icon: Icon(Icons.visibility)),
+                          hintText: "Enter your password",
                           border: OutlineInputBorder(
                               borderSide: BorderSide(color: Colors.blue),
-                              borderRadius: BorderRadius.circular(60))),
-                    ),
-                    SizedBox(height: 20),
-                    TextFormField(
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        prefix: Icon(Icons.lock),
-                        suffix: IconButton(
-                            onPressed: () {}, icon: Icon(Icons.visibility)),
-                        hintText: "Enter your password",
-                        border: OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.blue),
-                            borderRadius: BorderRadius.circular(60)),
+                              borderRadius: BorderRadius.circular(60)),
+                        ),
                       ),
-                    ),
-                    SizedBox(height: 20),
-                    Text("Forgot Password?", style: TextStyle(fontSize: 16)),
-                    Spacer(),
-                    MaterialButton(
-                        minWidth: 300,
-                        height: 45,
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(50)),
-                        color: Colors.blue,
-                        onPressed: () {},
-                        child: Text(
-                          "Sign in",
-                          style: TextStyle(color: Colors.white, fontSize: 20),
-                        )),
-                    Padding(
-                      padding: const EdgeInsets.only(bottom: 50),
-                      child: RichText(
-                          text: TextSpan(
-                              text: "Don't have an account?",
-                              style:
-                                  TextStyle(color: Colors.black, fontSize: 16),
-                              children: <TextSpan>[
-                            TextSpan(
-                              text: " Signup",
-                              style:
-                                  TextStyle(color: Colors.blue, fontSize: 18),
-                            )
-                          ])),
-                    )
-                  ],
+                      SizedBox(height: 20),
+                      Text("Forgot Password?", style: TextStyle(fontSize: 16)),
+                      Spacer(),
+                      MaterialButton(
+                          minWidth: 300,
+                          height: 45,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(50)),
+                          color: Colors.blue,
+                          onPressed: () {
+                            if (formkey.currentState!.validate()) {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => home()));
+                            }
+                          },
+                          child: Text(
+                            "Sign in",
+                            style: TextStyle(color: Colors.white, fontSize: 20),
+                          )),
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 50),
+                        child: RichText(
+                            text: TextSpan(
+                                text: "Don't have an account?",
+                                style: TextStyle(
+                                    color: Colors.black, fontSize: 16),
+                                children: <TextSpan>[
+                              TextSpan(
+                                text: " Signup",
+                                style:
+                                    TextStyle(color: Colors.blue, fontSize: 18),
+                              )
+                            ])),
+                      )
+                    ],
+                  ),
                 ),
               ),
             ),
